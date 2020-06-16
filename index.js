71cc1aed03996012b02a72bc46b474b6e2057318
Vipul Mahadik <vipulkumar.mahadik@servicenow.com>
2020-06-15 21:17:54 -0700

    Fix for Firebase
    npm install --save @google-cloud/firestore
    https://cloud.google.com/firestore/docs/quickstart-servers#node.js

--- index.js
+++ index.js
@@ -2,19 +2,19 @@
 const express = require('express');
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const routes = require('./routes/employeeRoutes'); //importing route
 
 // initialize express & bodyParser.
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(cors({ origin: true }));
 
 port = process.env.PORT || 3000;
-bodyParser = require('body-parser');
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
 // Register routes.
 routes(app);
